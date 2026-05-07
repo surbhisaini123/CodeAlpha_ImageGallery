c3a8f463f7db599420d7a09c7e25a49a12df8591
surbhisaini123 <shurbhisaini2006@gmail.com>
2026-05-07 22:32:55 +0530

    project

--- script.js
+++ script.js
@@ -2,39 +2,37 @@ let image=document.querySelectorAll(".inside img")
 let lightbox=document.querySelector(".lightbox")
 let lightboxImg=document.querySelector(".lightbox-img")
 let closebtn=document.querySelector(".close")
-let prevbtn=document.querySelector(".previous")
+let prevbtn=document.querySelector(".Previous")
+
 let nextbtn=document.querySelector(".next")
 
 let currentIndex=0
 for(let i=0;i<image.length;i++){
     image[i].onclick=function () {
-        console.log("hiii");
+        console.log("hiii big img is open");
           lightbox.style.display="flex"
         lightboxImg.src=this.src
         currentIndex=i
     }
         
-    
-    // addEventListener("click",()=>{
-    //     console.log("hiii");
-        
-    //     lightbox.style.display="flex"
-    //     lightboxImg.src=this.src
-    //     currentIndex=i
-
-
-    // })
+  
 
 }
 closebtn.addEventListener("click",()=>{
-    console.log("hiii");
+    console.log("hiii btn is close");
     lightbox.style.display="none"
 
 })
 
 nextbtn.addEventListener("click",()=>{
+    console.log("hiii click next btn");
     currentIndex=(currentIndex+1)%image.length
-    lightbox.src=image[currentIndex].src
+    lightboxImg.src=image[currentIndex].src
 })
-    
+
+prevbtn.addEventListener("click",()=>{
+    console.log("hiii close btn is click");
+    currentIndex=(currentIndex-1+image.length)%image.length
+     lightboxImg.src=image[currentIndex].src
+})
 
